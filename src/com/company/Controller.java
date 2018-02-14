4c4b68b5205f3d49d876ceb3381c00e0a3e73e91
CybberMuffin <33846592+CybberMuffin@users.noreply.github.com>
2018-02-14 16:53:34 +0200

    Update Controller.java

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -7,24 +7,7 @@ public class Controller {
 
         String userLine = in.nextLine();
         int userNumber = -1;
-
-        while (!isOnlyDigits(userLine) || (userNumber < 0 || userNumber > 100)) {
-            if (!isOnlyDigits(userLine)) {
-                View.message(View.INCORRECT_TYPE);
-                userLine = in.nextLine();
-            }
-            else {
-                userNumber = Integer.parseInt(userLine);
-                if ((userNumber < 0 || userNumber > 100)) {
-                    View.message(View.INCORRECT_DATA);
-                    userLine = in.nextLine();
-                }
-            }
-        }
-        return userNumber;
-    }
-
-    private static boolean isOnlyDigits(String str) {
-        return str.matches("[\\d]+");
+        
+        /*bags detected, update incoming*/
     }
 }
